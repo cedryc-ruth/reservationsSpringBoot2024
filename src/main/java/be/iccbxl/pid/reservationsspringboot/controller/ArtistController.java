62d3c42714b9755d8fb2f02715065145d21f1578
Cédryc Ruth <ceruth@epfc.eu>
2024-12-24 01:02:23 +0100

    Form update Artist (Validation not working)

--- src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
+++ src/main/java/be/iccbxl/pid/reservationsspringboot/controller/ArtistController.java
@@ -5,13 +5,21 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 
 import be.iccbxl.pid.reservationsspringboot.model.Artist;
 import be.iccbxl.pid.reservationsspringboot.service.ArtistService;
 
 @Controller
+@Validated
 public class ArtistController {
 	@Autowired
 	ArtistService service;
@@ -27,7 +35,7 @@ public class ArtistController {
     }
 
     @GetMapping("/artists/{id}")
-    public String show(Model model, @PathVariable("id") long id) {
+    public String show(Model model, @PathVariable long id) {
 	Artist artist = service.getArtist(id);
 
 	model.addAttribute("artist", artist);
@@ -35,5 +43,40 @@ public class ArtistController {
 		
         return "artist/show";
     }
+    
+	@GetMapping("/artists/{id}/edit")
+	public String edit(Model model, @PathVariable long id, HttpServletRequest request) {
+		Artist artist = service.getArtist(id);
+
+		model.addAttribute("artist", artist);
+
+		//Générer le lien retour pour l'annulation
+		String referrer = request.getHeader("Referer");
+
+		if(referrer!=null && !referrer.equals("")) {
+			model.addAttribute("back", referrer);
+		} else {
+			model.addAttribute("back", "/artists/"+artist.getId());
+		}
+		
+		return "artist/edit";
+	}
+	
+	@PutMapping("/artists/{id}/edit")
+	public String update(@Valid @ModelAttribute Artist artist, BindingResult bindingResult, @PathVariable long id, Model model) {
+		if (bindingResult.hasErrors()) {
+			return "artist/edit";
+		}
+		
+		Artist existing = service.getArtist(id);
+		
+		if(existing==null) {
+			return "artist/index";
+		}		
+	
+	    service.updateArtist(id, artist);	    
+    
+		return "redirect:/artists/"+artist.getId();
+	}
 
 }
